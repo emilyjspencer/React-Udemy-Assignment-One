ed5f6073270b400321d57508f238f3f00d581eef
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-07-16 15:05:34 +0100

    Refactors to use variable instead of a ternary operator because ternary operators should be used with caution

--- src/CharComponent/CharComponent.js
+++ src/CharComponent/CharComponent.js
@@ -3,7 +3,14 @@ import React from 'react';
 
 const CharComponent = () => {
 
-  
+    const style = {
+        display: inline-block,
+        padding: 16px, 
+        text-align: center, 
+        margin: 16px, 
+        border: 1px solid black
+    }
+    
   return (
       style={style}
   )
